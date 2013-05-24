0d72a9c9c9b54c5a5c6b21d0f71e090711015470
Fabian Bornhofen <fbornhofen@gmail.com>
2013-05-24 10:56:15 +0200

    sorting frequencies

--- gymnastics/go/vigenere_crack/main.go
+++ gymnastics/go/vigenere_crack/main.go
@@ -4,7 +4,28 @@ import (
 	"fmt"
 	"os"
 	"flag"
+	"sort"
 )
+
+type FreqTuple struct {
+	c byte
+	n int
+}
+
+type FreqTuples []*FreqTuple
+
+func (f FreqTuples) Less(i int, j int) bool {
+	// Sort in reverse order
+	return f[i].n > f[j].n
+}
+
+func (f FreqTuples) Swap(i int, j int) {
+	f[i], f[j] = f[j], f[i]
+}
+
+func (f FreqTuples) Len() int {
+	return len(f)
+}
 
 func stripBuffer(b []byte) []byte {
 	res := make([]byte, len(b))
@@ -28,6 +49,19 @@ func collectNth(b []byte, n int) []byte {
 	return res
 }
 
+func sortedByFrequency(dict map[byte]int) FreqTuples {
+	fs := make(FreqTuples, len(dict))
+	i := 0
+	for k, v := range dict {
+		fs[i] = new(FreqTuple)
+		fs[i].c = k
+		fs[i].n = v
+		i++
+	}
+	sort.Sort(fs)
+	return fs
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
@@ -35,6 +69,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "missing filename\n")
 		os.Exit(-1)
 	}
+
 	f, _ := os.Open(args[0])
 	buf := make([]byte, 1024)
 	freq := make(map[byte]int)
@@ -43,7 +78,8 @@ func main() {
 	for i := 0; i < len(stripped); i++ {
 		freq[stripped[i]]++
 	}
-	for k, v := range freq {
-		fmt.Printf("%c - %d\n", k, v)
+	sorted := sortedByFrequency(freq)
+	for _, v := range sorted {
+		fmt.Printf("%c - %d\n", v.c, v.n)
 	}
 }
