b74a11cd9be66fabd3129e08b8a5e6af8d7c4d43
Fabian Bornhofen <fbornhofen@gmail.com>
2013-05-28 16:53:22 +0200

    freq. analysis step 1

--- gymnastics/clojure/simplecrypto/src/simplecrypto/core.clj
+++ gymnastics/clojure/simplecrypto/src/simplecrypto/core.clj
@@ -17,3 +17,14 @@
                  0
                  (second params))]
        (apply str (take-nth n (drop offset s)))))
+
+(defn analyze-frequencies [s]
+  (let [freqs (frequencies s)
+        char-freq-pairs (seq freqs)
+        sorted-freqs (sort #(> (second %1) (second %2)) char-freq-pairs)]
+    sorted-freqs))
+
+(defn analyze []
+  (let [crypt-string "Xleg M gmnw qqtily mf xifpw pj mcsmcpe Xsliir Xejz chxik xo xi, Wpxloaok pzvvt oy aathof, dfx be tf. Tyh jr fj zpyr zj eerdyikt Sap at smlrvjrg cmyix by xssnm sx qe, Whfekbyk xsrwd gg wbdhgn, epx jx up. Pem ml fe, pwu im fw, Lxe au bx, dfx be tf, Psmkqir hsjew hq ojwdhx, mit tx ci. Efe wapr ule mvglin-sissxew twptlx Pawmnz mf xhx agspd lkjfi, Elwsi ptpd fe lr brsppv, pem ml fe. Jgs tazyyi tapc ney mi qermph Ulekp at smtpd e vsefdi msel xhxj ojpl diw, Tapvw aiew tf ag eftaek, dfx be tf. Wil mt mi, pem ml fe, Pwu im fw, lxe au bx, Liirx aamp up so agdaws, epx jx up. Mit tx ci, wil mt mi, Pem ml fe, pwu im fw, Watwhfv pzvvt oy aathof, dfx be tf."
+        stripped-string (strip-string crypt-string)]
+    (println stripped-string)))
