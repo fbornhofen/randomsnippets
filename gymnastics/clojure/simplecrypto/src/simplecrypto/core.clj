22f1c0f909f805799aed298084616924eb9af663
Fabian Bornhofen <fbornhofen@gmail.com>
2013-05-28 23:35:00 +0200

    helpers

--- gymnastics/clojure/simplecrypto/src/simplecrypto/core.clj
+++ gymnastics/clojure/simplecrypto/src/simplecrypto/core.clj
@@ -20,10 +20,19 @@
       []
       (apply str (take-nth n (drop offset s))))))
 
+(defn sorted-frequencies [s]
+  (let [freqs (frequencies s)
+        char-freq-pairs (seq freqs)
+        letter-freqs (filter (fn [tup] (is-letter (first tup))) char-freq-pairs)]
+    (sort #(> (second %1) (second %2)) letter-freqs)))
+
+(defn most-frequent-char [s]
+  (first (first (sorted-frequencies s))))
+
 (defn frequency-tuples [s]
   (let [freqs (frequencies s)
         char-freq-pairs (seq freqs)
-        sorted-freqs (sort #(> (second %1) (second %2)) char-freq-pairs)
+        sorted-freqs (sorted-frequencies s)
         total (reduce + (mapv second char-freq-pairs))]
     (mapv
      (fn [tup] [(first tup) (second tup) (float (/ (second tup) total))])
@@ -59,10 +68,11 @@
 (defn analyze []
   (let [crypt-string "Xleg M gmnw qqtily mf xifpw pj mcsmcpe Xsliir Xejz chxik xo xi, Wpxloaok pzvvt oy aathof, dfx be tf. Tyh jr fj zpyr zj eerdyikt Sap at smlrvjrg cmyix by xssnm sx qe, Whfekbyk xsrwd gg wbdhgn, epx jx up. Pem ml fe, pwu im fw, Lxe au bx, dfx be tf, Psmkqir hsjew hq ojwdhx, mit tx ci. Efe wapr ule mvglin-sissxew twptlx Pawmnz mf xhx agspd lkjfi, Elwsi ptpd fe lr brsppv, pem ml fe. Jgs tazyyi tapc ney mi qermph Ulekp at smtpd e vsefdi msel xhxj ojpl diw, Tapvw aiew tf ag eftaek, dfx be tf. Wil mt mi, pem ml fe, Pwu im fw, lxe au bx, Liirx aamp up so agdaws, epx jx up. Mit tx ci, wil mt mi, Pem ml fe, pwu im fw, Watwhfv pzvvt oy aathof, dfx be tf."
         stripped (strip-string crypt-string)
+        ;stripped crypt-string
         freqs (frequency-distributions stripped (/ (count stripped) 5))]
     (doall
      (map-indexed (fn [i f]
-                    (printf "%d\t" i)
+                    (printf "%d\t" (inc i))
                     (doall
                      (map (fn [val]
                             (printf "  %.4f" val))
