0b57eab1d6fe6b1e5c5c4d6a8928ff4714a5f787
Fabian Bornhofen <fbornhofen@gmail.com>
2013-05-28 17:54:17 +0200

    prettier output

--- gymnastics/clojure/simplecrypto/src/simplecrypto/core.clj
+++ gymnastics/clojure/simplecrypto/src/simplecrypto/core.clj
@@ -36,5 +36,8 @@
 
 (defn analyze []
   (let [crypt-string "Xleg M gmnw qqtily mf xifpw pj mcsmcpe Xsliir Xejz chxik xo xi, Wpxloaok pzvvt oy aathof, dfx be tf. Tyh jr fj zpyr zj eerdyikt Sap at smlrvjrg cmyix by xssnm sx qe, Whfekbyk xsrwd gg wbdhgn, epx jx up. Pem ml fe, pwu im fw, Lxe au bx, dfx be tf, Psmkqir hsjew hq ojwdhx, mit tx ci. Efe wapr ule mvglin-sissxew twptlx Pawmnz mf xhx agspd lkjfi, Elwsi ptpd fe lr brsppv, pem ml fe. Jgs tazyyi tapc ney mi qermph Ulekp at smtpd e vsefdi msel xhxj ojpl diw, Tapvw aiew tf ag eftaek, dfx be tf. Wil mt mi, pem ml fe, Pwu im fw, lxe au bx, Liirx aamp up so agdaws, epx jx up. Mit tx ci, wil mt mi, Pem ml fe, pwu im fw, Watwhfv pzvvt oy aathof, dfx be tf."
-        stripped-string (strip-string crypt-string)]
-    (println stripped-string)))
+        stripped (strip-string crypt-string)
+        freqs (analyze-strided-frequencies stripped (/ (count stripped) 5))]
+    (map-indexed (fn [i f]
+                   (println i)
+                   (println (str "  " (vec (take 4 f))))) freqs)))
