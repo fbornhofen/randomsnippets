93d42bbc1b3a0beb9d4988b622311c9d50dffdde
Fabian Bornhofen <fbornhofen@gmail.com>
2014-12-27 14:50:18 +0100

    add melody

--- audio_playground/pywave.py
+++ audio_playground/pywave.py
@@ -7,7 +7,6 @@ import sys
 
 f = wave.open(sys.argv[1], "w")
 framerate = 32768
-frequency = 440
 
 SAMPLEBITS=16
 SAMPLEMOD=2**SAMPLEBITS
@@ -16,12 +15,12 @@ f.setnchannels(1)
 f.setsampwidth(SAMPLEBITS / 8)
 f.setframerate(framerate)
 
-amplitude = SAMPLEMAX/2
-iterations = 32768
-a440Hz = framerate / math.pi / 2.0 / frequency
-notes = []
+amplitude = SAMPLEMAX/8
+iterations = 32768 / 7
+a440Hz = framerate / math.pi / 2.0 / 440
+notes = [99999] # pause
 step = math.pow(2, 1./12)
-for i in range(0,13):
+for i in range(0, 37):
     notes.append(a440Hz / step ** i)
 
 def sample_notes(frequencies, pos):
@@ -30,13 +29,18 @@ def sample_notes(frequencies, pos):
         res += int((amplitude * math.sin(float(pos)/freq))/2)
     return res
 
-sequence = [0, 2, 4, 5, 7, 9, 11, 12]
+def write_pause(wavefile, length):
+    for i in range(length):
+        f.writeframes(struct.pack('H', 0))
+
+sequence = [16, 14, 16, 11, 7, 11, 4, 0,
+            16, 14, 16, 11, 7, 11, 4]
 for i in sequence:
-    for j in range(0, iterations/2):
-        n1 = notes[i]
-        n2 = notes[(i+3)%12]
-        val = sample_notes([n1, n2], j)
+    n1 = notes[i]
+    for j in range(0, iterations):
+        val = sample_notes([n1], j)
         f.writeframes(struct.pack('H', val % SAMPLEMOD))
         # Optional: write 1 more value for 2nd channel
+    write_pause(f, iterations/3)
 
 f.close()
